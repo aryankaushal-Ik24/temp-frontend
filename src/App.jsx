1da4fd468c93a90165dc91c7da8966c39370ebf4
unknown <aryan kaushal>
2025-05-01 18:07:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const App = () => {
+  
+    const handleOnClick = (e) => {
+      e.preventDefault();
+      const shop = document.getElementById('shop').value;
+      const clientId = 'd58d81c5f8fa4c477cda2118c4ea0c2d';
+      const redirectUri = encodeURIComponent('https://sides-invasion-lifestyle-ag.trycloudflare.com/auth/callback');
+      const scopes = 'read_products';
+      const authUrl = `https://${shop}/admin/oauth/authorize?client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}`;
+      window.location.href = authUrl;
+    };
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <h1>click the button to get the connected with shopify</h1>
+      <div>
+        <form id="connect-form" onSubmit={handleOnClick}>
+          <input type="text" id="shop" placeholder="yourstore.myshopify.com" required />
+          <button type="submit">Connect Store</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
