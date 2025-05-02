b9edf7a7a569451be34db931a38e1298d48f223c
unknown <aryan kaushal>
2025-05-02 12:36:26 +0530

    user sent the store name

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const App = () => {
-  
+    const shopRef = useRef(null);
     const handleOnClick = (e) => {
       e.preventDefault();
-      const shop = 'aryan-store-007.myshopify.com'
+      const shop = shopRef.current.value;
       console.log(shop);
       const clientId = 'd58d81c5f8fa4c477cda2118c4ea0c2d';
       const redirectUri = encodeURIComponent('https://temp-backend-5.onrender.com/auth/callback');
@@ -12,14 +12,14 @@ const App = () => {
       const authUrl = `https://${shop}/admin/oauth/authorize?client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}`;
       console.log(authUrl);
       window.location.href = authUrl;
-    };
+    };  
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
       <h1>click the button to get the connected with shopify</h1>
       <div>
         <form id="connect-form" onSubmit={handleOnClick}>
-          <input type="text" id="shop" placeholder="yourstore.myshopify.com" required />
+          <input type="text" id="shop" placeholder="yourstore.myshopify.com" ref={shopRef} required />
           <button type="submit">Connect Store</button>
         </form>
       </div>
